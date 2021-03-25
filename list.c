c80a41b72bea8e5a5090693f1524d97d1e566730
Replit user <replituser@example.com>
2021-03-25 18:41:54 +0000

    new try

--- list.c
+++ list.c
@@ -105,6 +105,9 @@ void * popBack(List * list) {
 }
 
 void * popCurrent(List * list) {
+  if(list->current->next && list->current->prev != NULL){
+    list->current->prev = list->current->next;
+  }
     return NULL;
 }
 
